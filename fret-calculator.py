e08b00f143fb18981bb59d079f91079239338f7f
Backlund <backlund@tigge.org>
2019-10-26 23:35:49 +0200

    Clean up

--- fret-calculator.py
+++ fret-calculator.py
@@ -7,6 +7,6 @@ distance = 0
 for fret in range(1, numfrets+1):
     location = scale - distance
     scaling_factor = location / 17.817
-    distance = distance + scaling_factor
+    distance += scaling_factor
     print ("Distance to fret {} in mm: {}".format(fret, round(distance, 2)))
 
