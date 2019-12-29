cb9dd82cf618df7be312e807687e64efde62d02b
Backlund <backlund@tigge.org>
2019-12-29 01:07:38 +0100

    Added units

--- fret-calculator.py
+++ fret-calculator.py
@@ -1,12 +1,15 @@
 
-scale = float(input ("Scale in mm: "))
+scale = float(input ("Scale: "))
 numfrets = int(input("Number of frets: "))
 
 distance = 0
+units = "mm"
+if scale < 50:
+    units = "inches"
 
 for fret in range(1, numfrets+1):
     location = scale - distance
     scaling_factor = location / 17.817
     distance += scaling_factor
-    print ("Distance to fret {} in mm: {}".format(fret, round(distance, 2)))
+    print ("Distance to fret {} in {}: {}".format(fret, units, round(distance, 3)))
 
